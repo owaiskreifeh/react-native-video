8e2fe78f0aba0b3fc70720cd9e03ce124b65e82f
Yazeed Assad <yazeed.assad@mbc.net>
2020-12-25 21:52:09 +0200

    return 2 letter language code in tracks info

--- ios/Video/RCTVideo.m
+++ ios/Video/RCTVideo.m
@@ -1187,11 +1187,13 @@ static int const RCTVideoUnset = -1;
         if (values.count > 0) {
           title = [values objectAtIndex:0];
         }
-        NSString *language = [currentOption extendedLanguageTag] ? [currentOption extendedLanguageTag] : @"";
+        NSString *threeLanguage = [currentOption extendedLanguageTag] ? [currentOption extendedLanguageTag] : @"";
+        NSString *language = [[currentOption locale] languageCode] ?[[currentOption locale] languageCode]  : @"";
         NSDictionary *audioTrack = @{
                                      @"index": [NSNumber numberWithInt:i],
                                      @"title": title,
-                                     @"language": language
+                                     @"language": language,
+                                     @"three-letter-language": threeLanguage
                                      };
         [audioTracks addObject:audioTrack];
     }
@@ -1214,11 +1216,14 @@ static int const RCTVideoUnset = -1;
     if (values.count > 0) {
       title = [values objectAtIndex:0];
     }
-    NSString *language = [currentOption extendedLanguageTag] ? [currentOption extendedLanguageTag] : @"";
+   NSString *threeLanguage = [currentOption extendedLanguageTag] ? [currentOption extendedLanguageTag] : @"";
+   NSString *language = [[currentOption locale] languageCode] ?[[currentOption locale] languageCode]  : @"";
+    
     NSDictionary *textTrack = @{
                                 @"index": [NSNumber numberWithInt:i],
                                 @"title": title,
-                                @"language": language
+                                @"language": language,
+                                @"three-letter-language": threeLanguage
                                 };
     [textTracks addObject:textTrack];
   }
