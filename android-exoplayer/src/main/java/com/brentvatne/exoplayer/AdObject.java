c86d4c1e5226daa108a1f6c0e322cbc3582dac2b
suZaytoon <suleiman.zaytoon@shahid.net>
2022-10-10 22:33:17 +0300

    updating ad object

--- android-exoplayer/src/main/java/com/brentvatne/exoplayer/AdObject.java
+++ android-exoplayer/src/main/java/com/brentvatne/exoplayer/AdObject.java
@@ -2,12 +2,12 @@ package com.brentvatne.exoplayer;
 import java.util.ArrayList;
 
 public class AdObject {
-  public final long adStart;
-  public final long adEnd;
-  public final long adDuration;
-  public final boolean played;
-  public final boolean started;
-  public final ArrayList <AdObject> slotAds;
+  public  long adStart;
+  public  long adEnd;
+  public  long adDuration;
+  public  boolean played;
+  public  boolean started;
+  public  ArrayList <AdObject> slotAds;
 
   public AdObject(long adStart, long adEnd, long adDuration, boolean played, boolean started, ArrayList <AdObject> slotAds) {
       this.adStart = adStart;
@@ -17,4 +17,10 @@ public class AdObject {
       this.started =  started;
       this.slotAds = slotAds;
   }
+
+    public AdObject(long adStart, long adEnd, long adDuration) {
+        this.adStart = adStart;
+        this.adEnd = adEnd;
+        this.adDuration = adDuration;
+    }
 }
