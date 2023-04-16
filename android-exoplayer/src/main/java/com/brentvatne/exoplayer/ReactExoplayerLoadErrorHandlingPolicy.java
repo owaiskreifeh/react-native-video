fb55730b6ea67fbffb710b982dfcf0278c7dd2af
Suleiman.Zaytoon <Suleiman.Zaytoon@shahid.net>
2023-04-16 16:00:32 +0300

    making getMinimumLoadableRetryCount return 2

--- android-exoplayer/src/main/java/com/brentvatne/exoplayer/ReactExoplayerLoadErrorHandlingPolicy.java
+++ android-exoplayer/src/main/java/com/brentvatne/exoplayer/ReactExoplayerLoadErrorHandlingPolicy.java
@@ -16,19 +16,15 @@ public final class ReactExoplayerLoadErrorHandlingPolicy extends DefaultLoadErro
 
     @Override
     public long getRetryDelayMsFor(LoadErrorInfo loadErrorInfo) {
-        if (
-                loadErrorInfo.exception instanceof HttpDataSourceException &&
-                        (loadErrorInfo.exception.getMessage() == "Unable to connect" || loadErrorInfo.exception.getMessage() == "Software caused connection abort")
-        ) {
-            // Capture the error we get when there is no network connectivity and keep retrying it
-            return 1000; // Retry every second
-        } else {
+        if (loadErrorInfo.errorCount < this.minLoadRetryCount) {
+            return Math.min((loadErrorInfo.errorCount - 1) * 1000, 5000); // Default timeout handling
+        } else { 
             return C.TIME_UNSET; // Done retrying and will return the error immediately
         }
     }
 
     @Override
     public int getMinimumLoadableRetryCount(int dataType) {
-        return Integer.MAX_VALUE;
+        return 2;
     }
 }
