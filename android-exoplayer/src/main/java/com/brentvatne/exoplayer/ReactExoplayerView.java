b90c34185e58c3065f8f274f89ca5704a8150f85
Suleiman.Zaytoon <Suleiman.Zaytoon@shahid.net>
2023-05-07 16:04:12 +0300

    fixing ad timer not showing

--- android-exoplayer/src/main/java/com/brentvatne/exoplayer/ReactExoplayerView.java
+++ android-exoplayer/src/main/java/com/brentvatne/exoplayer/ReactExoplayerView.java
@@ -370,7 +370,7 @@ class ReactExoplayerView extends FrameLayout implements
                 }
 
                 // to start the AD
-                if (adObject.adStart >= pos && !adObject.started) {
+                if (adObject.adStart <= pos &&  adObject.adEnd >= pos && !adObject.started) {
                     WritableMap eventData = Arguments.createMap();
                     eventData.putInt("index", i);
                     adObject.started = true;
